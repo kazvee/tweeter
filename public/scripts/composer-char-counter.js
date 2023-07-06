9ac49f6c8b357100524ffb229be403fcf9fc5aa9
kazvee <kayvee123@gmail.com>
2023-07-06 10:23:38 -0600

    Add functionality to display characters remaining on page counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
   $(".new-tweet textarea").on("input", function() {
     const textLength = $(this).val().length;
     console.log("Characters left: 😃", 140 - textLength);
+    let charactersRemaining = 0;
+    charactersRemaining = parseInt($(this).parent().find(".counter").text(140 - textLength));
   });
 });
